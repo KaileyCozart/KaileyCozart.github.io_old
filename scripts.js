085ba42d71d777a10b26ca88fd21e2903b06b166
KaileyCozart <kaileync@comcast.net>
2020-08-16 18:08:19 -0700

    Redid website.

--- scripts.js
+++ scripts.js
@@ -12,14 +12,6 @@ $(document).ready(function() {
             $(".hideMe").addClass("visible");
             $(".hideMeParent").removeClass("experienceListItemHidden");
             $(".hideMeParent").addClass("experienceListItem");
-            $(".hideMe2").removeClass("visible");
-            $(".hideMe2").addClass("hidden");
-            $(".hideMeParent2").removeClass("experienceListItem");
-            $(".hideMeParent2").addClass("experienceListItemHidden");
-            $(".hideMe3").removeClass("visible");
-            $(".hideMe3").addClass("hidden");
-            $(".hideMeParent3").removeClass("experienceListItem");
-            $(".hideMeParent3").addClass("experienceListItemHidden");
         }
     });
     $(".toggle2").click(function(){
@@ -30,18 +22,10 @@ $(document).ready(function() {
             $(".hideMeParent2").addClass("experienceListItemHidden");
         }
         else if($(".hideMe2").hasClass("hidden")) {
-            $(".hideMe").removeClass("visible");
-            $(".hideMe").addClass("hidden");
-            $(".hideMeParent").removeClass("experienceListItem");
-            $(".hideMeParent").addClass("experienceListItemHidden");
             $(".hideMe2").removeClass("hidden");
             $(".hideMe2").addClass("visible");
             $(".hideMeParent2").removeClass("experienceListItemHidden");
             $(".hideMeParent2").addClass("experienceListItem");
-            $(".hideMe3").removeClass("visible");
-            $(".hideMe3").addClass("hidden");
-            $(".hideMeParent3").removeClass("experienceListItem");
-            $(".hideMeParent3").addClass("experienceListItemHidden");
         }
     });
     $(".toggle3").click(function(){
@@ -52,18 +36,139 @@ $(document).ready(function() {
             $(".hideMeParent3").addClass("experienceListItemHidden");
         }
         else if($(".hideMe3").hasClass("hidden")) {
-            $(".hideMe").removeClass("visible");
-            $(".hideMe").addClass("hidden");
-            $(".hideMeParent").removeClass("experienceListItem");
-            $(".hideMeParent").addClass("experienceListItemHidden");
-            $(".hideMe2").removeClass("visible");
-            $(".hideMe2").addClass("hidden");
-            $(".hideMeParent2").removeClass("experienceListItem");
-            $(".hideMeParent2").addClass("experienceListItemHidden");
             $(".hideMe3").removeClass("hidden");
             $(".hideMe3").addClass("visible");
             $(".hideMeParent3").removeClass("experienceListItemHidden");
             $(".hideMeParent3").addClass("experienceListItem");
+        }
+    });
+
+    $(".toggle4").click(function(){
+        if($(".hideMe4").hasClass("visible")) {
+            $(".hideMe4").removeClass("visible");
+            $(".hideMe4").addClass("hidden");
+            $(".hideMeParent4").removeClass("experienceListItem");
+            $(".hideMeParent4").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe4").hasClass("hidden")) {
+            $(".hideMe4").removeClass("hidden");
+            $(".hideMe4").addClass("visible");
+            $(".hideMeParent4").removeClass("experienceListItemHidden");
+            $(".hideMeParent4").addClass("experienceListItem");
+        }
+    });
+    $(".toggle5").click(function(){
+        if($(".hideMe5").hasClass("visible")) {
+            $(".hideMe5").removeClass("visible");
+            $(".hideMe5").addClass("hidden");
+            $(".hideMeParent5").removeClass("experienceListItem");
+            $(".hideMeParent5").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe5").hasClass("hidden")) {
+            $(".hideMe5").removeClass("hidden");
+            $(".hideMe5").addClass("visible");
+            $(".hideMeParent5").removeClass("experienceListItemHidden");
+            $(".hideMeParent5").addClass("experienceListItem");
+        }
+    });
+    $(".toggle6").click(function(){
+        if($(".hideMe6").hasClass("visible")) {
+            $(".hideMe6").removeClass("visible");
+            $(".hideMe6").addClass("hidden");
+            $(".hideMeParent6").removeClass("experienceListItem");
+            $(".hideMeParent6").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe6").hasClass("hidden")) {
+            $(".hideMe6").removeClass("hidden");
+            $(".hideMe6").addClass("visible");
+            $(".hideMeParent6").removeClass("experienceListItemHidden");
+            $(".hideMeParent6").addClass("experienceListItem");
+        }
+    });
+
+    $(".toggle7").click(function(){
+        if($(".hideMe7").hasClass("visible")) {
+            $(".hideMe7").removeClass("visible");
+            $(".hideMe7").addClass("hidden");
+            $(".hideMeParent7").removeClass("experienceListItem");
+            $(".hideMeParent7").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe7").hasClass("hidden")) {
+            $(".hideMe7").removeClass("hidden");
+            $(".hideMe7").addClass("visible");
+            $(".hideMeParent7").removeClass("experienceListItemHidden");
+            $(".hideMeParent7").addClass("experienceListItem");
+        }
+    });
+    $(".toggle8").click(function(){
+        if($(".hideMe8").hasClass("visible")) {
+            $(".hideMe8").removeClass("visible");
+            $(".hideMe8").addClass("hidden");
+            $(".hideMeParent8").removeClass("experienceListItem");
+            $(".hideMeParent8").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe8").hasClass("hidden")) {
+            $(".hideMe8").removeClass("hidden");
+            $(".hideMe8").addClass("visible");
+            $(".hideMeParent8").removeClass("experienceListItemHidden");
+            $(".hideMeParent8").addClass("experienceListItem");
+        }
+    });
+    $(".toggle9").click(function(){
+        if($(".hideMe9").hasClass("visible")) {
+            $(".hideMe9").removeClass("visible");
+            $(".hideMe9").addClass("hidden");
+            $(".hideMeParent9").removeClass("experienceListItem");
+            $(".hideMeParent9").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe9").hasClass("hidden")) {
+            $(".hideMe9").removeClass("hidden");
+            $(".hideMe9").addClass("visible");
+            $(".hideMeParent9").removeClass("experienceListItemHidden");
+            $(".hideMeParent9").addClass("experienceListItem");
+        }
+    });
+
+    $(".toggle10").click(function(){
+        if($(".hideMe10").hasClass("visible")) {
+            $(".hideMe10").removeClass("visible");
+            $(".hideMe10").addClass("hidden");
+            $(".hideMeParent10").removeClass("experienceListItem");
+            $(".hideMeParent10").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe10").hasClass("hidden")) {
+            $(".hideMe10").removeClass("hidden");
+            $(".hideMe10").addClass("visible");
+            $(".hideMeParent10").removeClass("experienceListItemHidden");
+            $(".hideMeParent10").addClass("experienceListItem");
+        }
+    });
+    $(".toggle11").click(function(){
+        if($(".hideMe11").hasClass("visible")) {
+            $(".hideMe11").removeClass("visible");
+            $(".hideMe11").addClass("hidden");
+            $(".hideMeParent11").removeClass("experienceListItem");
+            $(".hideMeParent11").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe11").hasClass("hidden")) {
+            $(".hideMe11").removeClass("hidden");
+            $(".hideMe11").addClass("visible");
+            $(".hideMeParent11").removeClass("experienceListItemHidden");
+            $(".hideMeParent11").addClass("experienceListItem");
+        }
+    });
+    $(".toggle12").click(function(){
+        if($(".hideMe12").hasClass("visible")) {
+            $(".hideMe12").removeClass("visible");
+            $(".hideMe12").addClass("hidden");
+            $(".hideMeParent12").removeClass("experienceListItem");
+            $(".hideMeParent12").addClass("experienceListItemHidden");
+        }
+        else if($(".hideMe12").hasClass("hidden")) {
+            $(".hideMe12").removeClass("hidden");
+            $(".hideMe12").addClass("visible");
+            $(".hideMeParent12").removeClass("experienceListItemHidden");
+            $(".hideMeParent12").addClass("experienceListItem");
         }
     });
     
